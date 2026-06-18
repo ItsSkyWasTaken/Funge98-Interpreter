a624a36f3ad2e33be95906557bb990fd9d2f7fef
Sky <62679043+ItsSkyWasTaken@users.noreply.github.com>
2026-06-18 15:24:25 -0500

    Add border shrinking for Unefunge

--- src/world/world.cpp
+++ src/world/world.cpp
@@ -240,17 +240,9 @@ void FungeWorld::put(const Vector& location, const char32_t value) {
         switch(dimensions) {
             case 1: {
                 if(location.getX() == low.getX()) {
-                    while(get(low) == U' ') {
-                        low += {1};
 
-                        if(low.getX() > high.getX()) {
-                            quit(0);
-                        }
-                    }
                 } else if(location.getX() == high.getX()) {
-                    while(get(high) == U' ') {
-                        high -= {1};
-                    }
+
                 }
 
                 break;
